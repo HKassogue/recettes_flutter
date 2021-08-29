b6109510d6f2c3350f11ea85c90916848cdae7c0
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-08-29 17:33:41 +0000

    changin the speed of transition (from button) for recipe screen when opening fron tap

--- lib/recipesListScreen.dart
+++ lib/recipesListScreen.dart
@@ -101,7 +101,8 @@ class RecipeItemWidget extends StatelessWidget {
               transitionsBuilder: (context, animation, secondaryAnimation, child) {
                 var begin = Offset(0.0, 1.0);
                 var end = Offset.zero;
-                var tween = Tween(begin: begin, end: end);
+                var curve = Curves.ease;
+                var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
                 return SlideTransition(
                   position: animation.drive(tween),
                   child: child,
