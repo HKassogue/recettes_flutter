447261372256dc7b78cc5ba99954084e1aabd3f0
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-08-31 18:42:03 +0000

    add of fields author, image URL, ingredients and preparation in recipeFormScreen.dart

--- lib/recipeFormScreen.dart
+++ lib/recipeFormScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mon1erprojet/recipe.dart';
 
 class RecipeFormScreen extends StatefulWidget {
   @override
@@ -9,14 +10,19 @@ class RecipeFormScreen extends StatefulWidget {
 class _RecipeFormScreenState extends State<RecipeFormScreen> {
   final formKey = GlobalKey<FormState>();
   final titleController = TextEditingController();
-  final descriptionController = TextEditingController();
-  final userController = TextEditingController();
+  final authorController = TextEditingController();
+  final urlController = TextEditingController();
+  final ingredientsController = TextEditingController();
+  final preparationController = TextEditingController();
+
 
   @override
   void dispose() {
     titleController.dispose();
-    descriptionController.dispose();
-    userController.dispose();
+    authorController.dispose();
+    urlController.dispose();
+    ingredientsController.dispose();
+    preparationController.dispose();
     super.dispose();
   }
 
@@ -45,7 +51,97 @@ class _RecipeFormScreenState extends State<RecipeFormScreen> {
                 ),
                 validator: (value) {
                   if (value!.isEmpty) {
-                    return 'Please enter a title';
+                    return 'Veuillez entrer un titre';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 16.0, left: 16.0, right: 16.0, bottom: 8.0
+              ),
+              child: TextFormField(
+                controller: authorController,
+                decoration: InputDecoration(
+                  labelText: 'Author',
+                  border: OutlineInputBorder(
+                    borderRadius:
+                    BorderRadius.all(Radius.circular(20.0))
+                  )
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Veuillez entrer l'auteur";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 16.0, left: 16.0, right: 16.0, bottom: 8.0
+              ),
+              child: TextFormField(
+                controller: urlController,
+                decoration: InputDecoration(
+                  labelText: 'ImageURL',
+                  border: OutlineInputBorder(
+                    borderRadius:
+                    BorderRadius.all(Radius.circular(20.0))
+                  )
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Veuillez entrer le lien de la photo";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 16.0, left: 16.0, right: 16.0, bottom: 8.0
+              ),
+              child: TextFormField(
+                controller: ingredientsController,
+                decoration: InputDecoration(
+                  labelText: 'Ingredients',
+                  border: OutlineInputBorder(
+                    borderRadius:
+                    BorderRadius.all(Radius.circular(20.0))
+                  )
+                ),
+                keyboardType: TextInputType.multiline,
+                minLines: 3,
+                maxLines: 5,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Veuillez entrer les ingredients';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 16.0, left: 16.0, right: 16.0, bottom: 8.0
+              ),
+              child: TextFormField(
+                controller: preparationController,
+                decoration: InputDecoration(
+                  labelText: 'Preparation',
+                  border: OutlineInputBorder(
+                    borderRadius:
+                    BorderRadius.all(Radius.circular(20.0))
+                  )
+                ),
+                keyboardType: TextInputType.multiline,
+                minLines: 3,
+                maxLines: 5,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Veuillez entrer la preparation';
                   }
                   return null;
                 },
